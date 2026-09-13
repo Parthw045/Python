a76ca9dc3fc5010aa25897b47a645ea435aaaf34
Parthw045 <wankhades812@gmail.com>
2026-09-13 23:07:25 +0530

    practicing python

---
+++ AreaOfCircle.py
@@ -0,0 +1,3 @@
+r = int(input("Enter radius of Circle:- "))
+area = 3.14 *r*r
+print("Area of Circle :-",area)
---
+++ CelsiusToFarheneit.py
@@ -0,0 +1,4 @@
+print("Welcome to Temperature converter!!")
+temp = int(input("Enter temperature in Celsius :-"))
+Farh = temp * 33.8
+print(f"Temperature = {Farh} F")
